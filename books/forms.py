28db0e6d96e5c330b612091fd13de4cd6cd364b0
Yaroslav <yaroslav.zhuvanov.ddi@gmail.com>
2020-09-21 17:38:28 +0300

    added some improvement

--- books/forms.py
+++ books/forms.py
@@ -4,6 +4,16 @@ from books.models import Book
 
 
 class BookForm(forms.ModelForm):
+    publication_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+    book_type = forms.ChoiceField(
+        required=False,
+        choices=[('', '----'), ] + Book.BookTypes.choices,
+        widget=forms.Select(attrs={'class': "custom-select"}),
+    )
+
     class Meta:
         model = Book
         fields = ('title', 'publication_date', 'author', 'price', 'pages', 'book_type', )
