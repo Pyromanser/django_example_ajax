a9085bef780fb6ac86e1319b01acfbc76f3c11e9
yaroslav <qwaswexexortinvoke@gmail.com>
2023-04-29 13:33:14 +0300

    update

--- books/views.py
+++ books/views.py
@@ -28,6 +28,25 @@ def save_book_form(request, form, template_name):
     return JsonResponse(data)
 
 
+# def book_create(request):
+#     data = dict()
+#     if request.method == 'POST':
+#         form = BookForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             data['form_is_valid'] = True
+#             books = Book.objects.all()
+#             data['html_book_list'] = render_to_string('books/includes/partial_book_list.html', {
+#                 'books': books
+#             })
+#         else:
+#             data['form_is_valid'] = False
+#     else:
+#         form = BookForm()
+#     data['html_form'] = render_to_string('books/includes/partial_book_create.html', {'form': form}, request=request)
+#     return JsonResponse(data)
+
+
 def book_create(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
